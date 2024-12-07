a19f83807327657317b340a719ad53a103b8c984
jump6746 <jump6746@naver.com>
2024-12-08 01:45:03 +0900

    미해결

--- week1/김종명/주사위 고르기.java
+++ week1/김종명/주사위 고르기.java
@@ -2,17 +2,14 @@ package programmers;
 
 import java.util.Arrays;
 
-public class Solution {
+// DFS * 3
 
-	static int n;
+// 가지 치기 필요 
 
-	static int[] answer;
-	static int[] player;
-	static int[] challenger;
-	static int[] playerScore;
-	static int[] challengerScore;
-	static double maxPercent;
-	
+// 50.0 / 100.0
+
+public class 주사위게임 {
+
 	public static void main(String[] args) {
 		
 		int[][] dice = {{1,2,3,4,5,6}, {3,3,3,3,4,4}, {1,3,3,4,4,4}, {1,1,4,4,5,5}};
@@ -21,45 +18,100 @@ public class Solution {
 
 	}
 	
+	static int n;
+	static int[] answer;
+	static int[] player;
+	static int[] challenger;
+	static int count;
+	static int maxCount;
+	
 	static public int[] solution(int[][] dice) {
 		
 		n = dice.length;
 		
 		player = new int[n/2];
 		challenger = new int[n/2];
+
+		maxCount = -1;
 		
-		int scoreLength = (int) Math.pow(6, n/2);
-		playerScore = new int[scoreLength];
-		challengerScore = new int[scoreLength];
+		DFS(0, 0, dice);
 		
-		maxPercent = -1;
-		
-		answer = DFS(0, 0);
+		for(int i=0; i<answer.length; i++) {
+			answer[i] += 1;
+		}
 		
 		return answer;
 	}
 	
-	static int[] DFS(int depth, int start) {
+	static void playerScore(int depth, int total, int[][] dice) {
 		
-		if(depth == player.length) {
+		if(depth == n/2) {
 			
-			System.out.println(Arrays.toString(player));
+			challengerScore(0, 0, dice, total);
 			
-			findChallenger(player);
-			
-			return new int[] {0};
+			return;
 		}
 		
+		for(int i=0; i<6; i++) {
+			
+			playerScore(depth + 1, total + dice[player[depth]][i], dice);
+			
+		}
+		
+	}
+	
+	static void challengerScore(int depth, int total, int[][] dice, int player) {
+		
+		if(total >= player) {
+			return;
+		}
+		
+		if(depth == n/2) {
+			
+			if(total < player) {
+				
+				count += 1;
+				
+			}
+			
+			return;
+		}
+		
+		for(int i=0; i<6; i++) {
+			
+			challengerScore(depth + 1, total + dice[challenger[depth]][i], dice, player);
+			
+		}
+		
+	}
+	
+	static void DFS(int depth, int start, int[][] dice) {
+		
+		if(depth == player.length) {
+					
+			findChallenger(player);
+			
+			count = 0;
+			
+			playerScore(0, 0, dice);
+			
+			if(count > maxCount) {
+				
+				maxCount = count;
+				
+				answer = Arrays.copyOf(player, player.length);
+			}
+			
+			return;
+		}
 		
 		for(int i=start; i<n; i++) {
 			
 			player[depth] = i;
 			
-			DFS(depth + 1, i + 1);
+			DFS(depth + 1, i + 1, dice);
 			
 		}
-		
-		return null;
 		
 	}
 	
