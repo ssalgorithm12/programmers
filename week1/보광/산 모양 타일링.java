ffcd54320055623e2081baf4bf9e90a59f83e05d
장보광 <154445682+JangBogwang@users.noreply.github.com>
2024-12-09 09:51:05 +0900

    해결

--- week1/보광/산 모양 타일링.java
+++ week1/보광/산 모양 타일링.java
@@ -6,15 +6,12 @@ class Solution {
         // dp[n][1]은 n+1번째 아래 타일 
       
         dp[0][1] = 1;
-        if(tops[0]==1) dp[1][0] = 3;
-        else dp[1][0] = 2; 
-        dp[1][1] = dp[1][0] + dp[0][1];
+        dp[1][0] = 2 + tops[0]; 
+        dp[1][1] = dp[1][0] + 1;
         
         for(int i = 2; i<= n; i++){
-            // 위 타일 dp 계산 
-            int check = tops[i-1];
-            if(check == 1) dp[i][0] = dp[i-1][0] + dp[i-1][1] * 2;
-            else dp[i][0] = dp[i-1][0] + dp[i-1][1];  
+            // 위 타일 dp 계산
+            dp[i][0] = dp[i-1][0] + dp[i-1][1]*(1+tops[i-1]);  
             if(dp[i][0] >= 10007) dp[i][0] %= 10007;
 
             // 아래 타일 dp 계산
