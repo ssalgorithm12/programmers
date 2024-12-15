4012a1943dba00f963df1397ebb181f946d23b0a
Gaeun999999 <bge524@naver.com>
2024-12-15 20:39:19 +0900

    미해결/40.9

--- week2/백가은/표 병합.java
+++ week2/백가은/표 병합.java
@@ -19,7 +19,7 @@ class Solution {
                 parent[i][j] = new P(i, j);
             }
         }
-        int n = commands.length();
+        int n = commands.length;
         for(int i = 0; i < n; i++) {
             st = new StringTokenizer(commands[i]);
             String comm = st.nextToken();
@@ -39,16 +39,14 @@ class Solution {
                     changeByValue(val1, val2);
                 }
                 
-            } else if(comm.eqauls("MERGE")) {
+            } else if(comm.equals("MERGE")) {
                 int r1 = Integer.parseInt(st.nextToken());
                 int c1 = Integer.parseInt(st.nextToken());
                 int r2 = Integer.parseInt(st.nextToken());
                 int c2 = Integer.parseInt(st.nextToken());
-                P p1 = new P(r1, c1);
-                P p2 = new P(r2, c2);
-                union(p1, p2);
+                merge(new P(r1, c1), new P(r2, c2));
                 
-            } else if(comm.eqauls("UNMERGE")) {
+            } else if(comm.equals("UNMERGE")) {
                 int r = Integer.parseInt(st.nextToken());
                 int c = Integer.parseInt(st.nextToken());
                 unmerge(new P(r, c));
@@ -56,14 +54,14 @@ class Solution {
             } else {
                 int r = Integer.parseInt(st.nextToken());
                 int c = Integer.parseInt(st.nextToken());
-                P parent = find(new P(r, c));
-                String val = map[parent.r][parent.c];
-                if(val == null) list.add("EMPTY");
-                else list.add(val);
+                P p = find(new P(r, c));
+                String val = map[p.r][p.c];
+                if(val == null) ansList.add("EMPTY");
+                else ansList.add(val);
             }
         }
         int size = ansList.size();
-        int[] answer = new int[size];
+        String[] answer = new String[size];
         for(int i = 0; i < size; i++) {
             answer[i] = ansList.get(i);
         }
@@ -71,45 +69,61 @@ class Solution {
     }
     
     static boolean isNumber(char c) {
-        if(c - '0' >= 0 && c - '0' <= 50) return true;
+        if(c - '0' >= 0 && c - '0' <= 9) return true;
         return false;
     }
     
     static void changeByLoc(P p, String val){
-        
+        p = find(p);
+        map[p.r][p.c] = val;
     }
     
     static void changeByValue(String val1, String val2) {
-        
-    }
-    
-    static void union(P p1, P p2) {
-        
-        P p1 = find(p1);
-        P p2 = find(p2);
-        
-        if(!isSame(find1, find2)) {
-            String val1 = map[p1.r][p1.c];
-            String val2 = map[p2.r][p2.c];
-
-            if(val1 != null) {
-                p[p2.r][p2.c] = new P(p1.r, p1.c);
-
-            } else if(val1 == null && val2 != null) {
-                p[p1.r][p1.c] = new P(p2.r, p2.c);          
+        for(int i = 1; i <= 50; i++) {
+            for(int j = 1; j <= 50; j++) {
+                P p = find(new P(i, j));
+                if(map[p.r][p.c] != null && map[p.r][p.c].equals(val1)) {
+                    map[p.r][p.c] = val2;
+                }
             }
         }
     }
     
-    static void unmerge(P p) {
-        P parent = find(p);
-        String val = map[parent.r][parent.c];
+    static void merge(P p1, P p2) {
+        p1 = find(p1);
+        p2 = find(p2);
         
+        if(!isSame(p1, p2)) {
+            String val1 = map[p1.r][p1.c];
+            String val2 = map[p2.r][p2.c];
+
+            if(val1 != null) {
+                parent[p2.r][p2.c] = p1;
+
+            } else if(val1 == null && val2 != null) {
+                parent[p1.r][p1.c] = p2;          
+            }
+        }
+    }
+    
+    // 부모가 분산됐을 수 있음?
+    static void unmerge(P p) {
+        P par = find(p);
+        String val = map[par.r][par.c] != null ? new String(map[par.r][par.c]) : null;
+        for(int i = 1; i <= 50; i++) {
+            for(int j = 1; j <= 50; j++) {
+                if(isSame(find(new P(i, j)), par)) {
+                    parent[i][j] = new P(i, j);
+                    if(isSame(new P(i, j), p)) map[i][j] = val;
+                    else map[i][j] = null;
+                }
+            }
+        }
     }
     
     static P find(P p) {
-        if(p[p.r][p.c].r == p.r && p[p.r][p.c].c == p.c) return p;
-        p[p.r][p.c] = find(p[p.r][p.c]);
+        if(parent[p.r][p.c].r == p.r && parent[p.r][p.c].c == p.c) return p;
+        return parent[p.r][p.c] = find(parent[p.r][p.c]);
     }
     
     static boolean isSame(P p1, P p2) {
