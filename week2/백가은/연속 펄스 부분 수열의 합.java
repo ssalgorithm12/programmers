8ab2c0809c1d1c040a00a466ff9bd93e2393b2cf
Gaeun999999 <bge524@naver.com>
2024-12-18 18:02:49 +0900

    해결/보광님 풀이 참고

--- week2/백가은/연속 펄스 부분 수열의 합.java
+++ week2/백가은/연속 펄스 부분 수열의 합.java
@@ -1,7 +1,27 @@
 class Solution {
-    
-    // dp인 것 같은데 구현 못 함 
-    public long solution(int[] arr) {
+    public long solution(int[] sequence) {
         
+        long[] sum = new long[sequence.length];
+        sum[0] = sequence[0];
+        
+        long answer = 0;
+        long max = 0;
+        long min = 0;
+        
+        answer = Math.abs(sequence[0]);
+        
+        for(int i = 1; i < sequence.length; i++){
+            
+            min = Math.min(min, sum[i-1]);
+            max = Math.max(max, sum[i-1]);
+            
+            if(i % 2 == 1) sum[i] = sum[i-1] - sequence[i]; 
+            else sum[i] = sum[i-1] + sequence[i]; 
+            
+            if(answer < sum[i] - min) answer = sum[i] - min;
+            if(answer < -(sum[i] - max)) answer = -(sum[i] - max);
+        }
+        
+        return answer;
     }
 }
