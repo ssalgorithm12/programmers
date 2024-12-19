8591a9679cd33f678f5f92982f6dcccebf1bd4c2
Gaeun999999 <bge524@naver.com>
2024-12-19 14:21:19 +0900

    미해결/시간초과

--- week3/백가은/2차원 동전 뒤집기.java
+++ week3/백가은/2차원 동전 뒤집기.java
@@ -1,44 +1,46 @@
 import java.util.*;
 
+// 백트래킹
 class Solution {
     static int h, w, min;
-    static int[][] target;
+    static int[][] map, target;
     static boolean[] visitedRow, visitedCol;
     
     public int solution(int[][] beginning, int[][] target) {
         min = Integer.MAX_VALUE;
         h = beginning.length;
         w = beginning[0].length;
+        map = beginning;
         this.target = target;
         
         visitedRow = new boolean[h];
         visitedCol = new boolean[w];
         
-        backtracking(beginning, new int[]{0, 0}, 0);
+        backtracking(new int[]{0, 0}, 0);
         
         if(min == Integer.MAX_VALUE) return -1;
         else return min;
     }
     
-    static void backtracking(int[][] map, int[] loc, int cnt) {
+    static void backtracking(int[] loc, int cnt) {
         
-        System.out.println(loc[0] + ", " + loc[1]);
-        
-        if(cnt >= min) return;
-        if(loc[0] == h && loc[1] == w) {
-            if(isValid(map)) min = cnt;
+        if(cnt >= min) return;                      // 가지치기
+        if(loc[1] == w) {                           // 다음 행으로 가기
+            backtracking(new int[]{loc[0] + 1, 0}, cnt);
+        }
+        if(loc[0] == h) {                           // 종료조건
+            if(isValid()) min = cnt;
             return;
             
-        } else if(loc[1] == w) {
-            backtracking(map, new int[]{loc[0] + 1, 0}, cnt);
         }
         
+        // 배열 순회하며 목표 상태와 다른 칸이 있으면 행 또는 열 뒤집기
         for(int i = loc[0]; i < h; i++) {
             for(int j = loc[1]; j < w; j++) {
                 
                 // 현재 칸의 상태가 목표 상태와 같은 경우 뒤집지 않고 다음 칸으로 넘어가기
                 if(map[i][j] == target[i][j]) {
-                    backtracking(map, new int[]{i, j + 1}, cnt);
+                    backtracking(new int[]{i, j + 1}, cnt);
                     
                 // 현재 칸의 상태가 목표 상태와 다른 경우
                 } else {
@@ -51,7 +53,8 @@ class Solution {
                         int[] copyRow = Arrays.copyOf(map[i], w);
                         
                         // 뒤집기
-                        backtracking(change(map, 0, i), new int[]{i, j + 1}, cnt + 1);
+                        change(0, i);
+                        backtracking(new int[]{i, j + 1}, cnt + 1);
 
                         // 상태 돌려놓기
                         visitedRow[i] = false;
@@ -69,25 +72,22 @@ class Solution {
                         }
                         
                         // 현재 열 뒤집기
-                        backtracking(change(map, 1, j), new int[]{i, j + 1}, cnt + 1);
+                        change(1, j);
+                        backtracking(new int[]{i, j + 1}, cnt + 1);
 
                         // 상태 돌려놓기
                         visitedCol[j] = false;
                         for(int k = 0; k < h; k++) {
                             map[k][j] = copyCol[k];
                         }
-                    } 
-
-                    // 뒤집지 않고 다음 칸으로 넘어가기
-                    backtracking(map, new int[]{i, j + 1}, cnt);
+                    }
                 }
+                
             }
         }
-        
     }
     
-    static int[][] change(int[][] map, int dir, int n) {
-        
+    static void change(int dir, int n) {
         if(dir == 0) {
             for(int i = 0; i < w; i++) {
                 map[n][i] = 1 - map[n][i];
@@ -97,13 +97,10 @@ class Solution {
             for(int i = 0; i < h; i++) {
                 map[i][n] = 1 - map[i][n];
             }
-            
         }
-        
-        return map;
     }
     
-    static boolean isValid(int[][] map){
+    static boolean isValid(){
         for(int i = 0; i < h; i++) {
             for(int j = 0; j < w; j++) {
                 if(map[i][j] != target[i][j]) return false;
