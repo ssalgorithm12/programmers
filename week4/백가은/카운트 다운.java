c371550eeaf54a29b6f0d57ab6241058c93260e4
Gaeun999999 <bge524@naver.com>
2024-12-28 19:07:41 +0900

    해결/65.75ms

--- week4/백가은/카운트 다운.java
+++ week4/백가은/카운트 다운.java
@@ -1,57 +1,41 @@
 import java.util.*;
 
+// Tabulation
 class Solution {
-    
-    static int[][] dp;
-    static List<Integer> scores;
-
     public int[] solution(int target) {
-        dp = new int[target + 1][2];
 
-        // 초기값 설정
-        dp[0][0] = 0; // 다트 수
-        dp[0][1] = 0; // 싱글 또는 불 횟수
-
-        // 점수 계산
-        for (int i = 1; i <= target; i++) {
-            dp[i][0] = Integer.MAX_VALUE;   // 다트 수 최댓값으로 초기화
-            dp[i][1] = 0;                   // 싱글 또는 불 횟수 초기화
+        // dp 배열 초기화
+        int[][] dp = new int[target + 1][2];
+        dp[0][0] = 0;
+        dp[0][1] = 0;
+        for(int i = 1; i <= target; i++) {
+            dp[i][0] = Integer.MAX_VALUE;
         }
 
-        // 다트로 얻을 수 있는 점수 저장
-        scores = new ArrayList<>();
-        scores.add(50); // 불
-        for (int i = 1; i <= 20; i++) {
-            scores.add(i);       // 싱글
-            scores.add(i * 2);   // 더블
-            scores.add(i * 3);   // 트리플
+        // 다트로 만들 수 있는 점수
+        List<Integer> scores = new ArrayList<>();
+        scores.add(50);
+        for(int i = 1; i <= 20; i++) {
+            scores.add(i);
+            scores.add(i * 2);
+            scores.add(i * 3);
         }
-
-        // Top-Down
-        memo(target);
-
-        return dp[target];
-    }
-
-    static int[] memo(int cur) {
-        if (cur < 0) return new int[]{Integer.MAX_VALUE, 0}; // 불가능한 점수
-        if (dp[cur][0] < Integer.MAX_VALUE) return dp[cur];  // 이미 계산된 경우
-
-        for (int score : scores) {
-            if (cur - score >= 0) {
-                int[] prev = memo(cur - score);
-                int dartCount = prev[0] + 1;
-                int specialCount = prev[1] + (score == 50 || score <= 20 ? 1 : 0);
-
-                // 더 적은 다트 수이거나, 같을 경우 싱글/불 횟수 비교
-                if (dartCount < dp[cur][0] || 
-                    (dartCount == dp[cur][0] && specialCount > dp[cur][1])) {
-                    dp[cur][0] = dartCount;
-                    dp[cur][1] = specialCount;
+        
+        // bottom-up
+        for(int i = 1; i <= target; i++) {
+            for(int s : scores) {
+                if(i - s < 0) continue;                                         // 현재 점수보다 큰 경우 continue;
+                int cnt = dp[i - s][0] + 1;                                     // 다트 수
+                int special = dp[i - s][1] + (s == 50 || s <= 20 ? 1 : 0);      // 불 또는 싱글 수
+                
+                // 다트 수가 적거나 불/싱글 수가 많은 경우 갱신
+                if(cnt < dp[i][0] || 
+                  (cnt == dp[i][0] && special > dp[i][1])) {
+                    dp[i][0] = cnt;
+                    dp[i][1] = special;
                 }
             }
         }
-
-        return dp[cur];
+        return dp[target];
     }
 }
