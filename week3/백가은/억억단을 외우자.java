f71f4af7857d5f23c36e61524cdbf51c4610e3da
Gaeun999999 <bge524@naver.com>
2024-12-29 13:46:41 +0900

    해결/326.26ms

--- week3/백가은/억억단을 외우자.java
+++ week3/백가은/억억단을 외우자.java
@@ -1,5 +1,39 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int e, int[] starts) {
-        // dp는 어려워요
+        
+        int len = starts.length;
+        
+        int[] res = new int[e + 1];
+        for(int i = 1; i <= e; i++) {
+            for(int j = 1; j <= e; j++) {
+                if(i * j <= e) res[i * j]++;
+                else break;
+            }
+        }
+        
+        int lower = Integer.MAX_VALUE;
+        for(int i = 0; i < len; i++) {
+            if(starts[i] < lower) lower = starts[i];
+        }
+        
+        int[] frq = new int[e + 1];
+        int max = -1;
+        int idx = -1;
+        for(int i = e; i >= lower; i--) {
+            if(res[i] >= max) {
+                idx = i;
+                max = res[i];
+            }
+            frq[i] = idx;
+        }
+        
+        int[] ans = new int[len];
+        for(int i = 0; i < len; i++) {
+            ans[i] = frq[starts[i]];
+        }
+        
+        return ans;
     }
 }
