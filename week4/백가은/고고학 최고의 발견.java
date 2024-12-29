e970ecf869f2037db178278c804fd65b82cddf94
Gaeun999999 <bge524@naver.com>
2024-12-29 16:33:44 +0900

    미해결

--- week4/백가은/고고학 최고의 발견.java
+++ week4/백가은/고고학 최고의 발견.java
@@ -3,6 +3,7 @@ import java.util.*;
 class Solution {
     static int n, min;
     static int[][] map;
+    
     static int[] dx = {0, -1, 0, 1};
     static int[] dy = {-1, 0, 1, 0};
     
@@ -11,42 +12,54 @@ class Solution {
         n = map.length;
         min = Integer.MAX_VALUE;
         
-        backtracking(new int[]{0, 0}, 0);
+        backtracking(0, 0, 0);
         return min;
     }
     
-    static void backtracking(int[] loc, int cnt) {
-        if(cnt >= min) return;
-        if(loc[1] == n) backtracking(new int[]{loc[0] + 1, 0}, cnt);
-        if(loc[0] == n) {
-            if(isValid()) min = cnt;
+    static void backtracking(int x, int y, int cnt) {
+        
+        if(cnt >= min) return;                      // 가지치기
+        
+        if(y == n) backtracking(x + 1, 0, cnt);     // 다음 행으로 넘어가기
+        else if(x == n) {                           // 종료 조건
+            if(isValid()) min = cnt;                // 모두 12시가 된 경우 최소값 갱신
             return;
         }
         
-        for(int i = loc[0]; i < n; i++) {
-            for(int j = loc[1]; j < n; j++) {
-                
-                if(map[i][j] == 0) backtracking(new int[]{i, j + 1}, cnt);
-                
-                int[][] copy = new int[n][n];
-                for(int k = 0; k < n; k++) {
-                    copy[k] = Arrays.copyOf(map[k], n);
+        else if(map[x][y] == 0) backtracking(x, y + 1, cnt);    // 이미 12시인 경우 넘어가기
+        else {                                                  // 12시가 아닌 경우 회전
+            
+            int[][] copy = new int[n][n];                       // 원래 상태 복사
+            for(int i = x - 1; i <= x + 1; i++) {
+                for(int j = y - 1; j <= y + 1; j++) {
+                    if(!check(i, j)) continue;
+                    copy[i][j] = map[i][j];
                 }
+            }
+            
+            // 회전 횟수
+            for(int i = 1; i <= 3; i++) {
                 
-                map[i][j] = (map[i][j] + 1) % 4;
-                for(int k = 0; k < 4; k++) {
-                    int nx = i + dx[k];
-                    int ny = j + dy[k];
+                map[x][y] = (map[x][y] + i) % 4;
+                
+                for(int j = 0; j < 4; j++) {    
+                    int nx = x + dx[j];
+                    int ny = y + dy[j];
                     
                     if(!check(nx, ny)) continue;
-                    map[nx][ny] = (map[nx][ny] + 1) % 4;
+                    map[nx][ny] = (map[nx][ny] + i) % 4;
                 }
                 
-                backtracking(new int[]{i, j + 1}, cnt + 1);
+                backtracking(x, y + 1, cnt + i);
                 
-                for(int k = 0; k < n; k++) {
-                    map[k] = copy[k];
+                for(int k = x - 1; k <= x + 1; k++) {           // 상태 복구
+                    for(int l = y - 1; l <= y + 1; l++) {
+                        if(!check(k, l)) continue;
+                        map[k][l] = copy[k][l];
+                    }
                 }
+                
+                if(map[x][y] == 0) break;
             }
         }
     }
