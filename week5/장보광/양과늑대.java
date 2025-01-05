fa564387075a770ef39558f8fea38d7171190264
장보광 <154445682+JangBogwang@users.noreply.github.com>
2025-01-06 01:20:46 +0900

    해결/4.06ms

--- week5/장보광/양과늑대.java
+++ week5/장보광/양과늑대.java
@@ -2,14 +2,15 @@ import java.util.*;
 
 class Solution {
     int answer = 0;
-    Deque<Integer> dq = new LinkedList<>();
-    List<Integer>[] arr;
-    boolean[] visited;
+    Deque<Integer> dq = new LinkedList<>(); // 이동 가능한 노드들 저장 
+    List<Integer>[] arr; // 트리 
+    boolean[] visited; // 방문 처리 
     
     public int solution(int[] info, int[][] edges) {
         arr = new ArrayList[info.length];
         visited = new boolean[info.length];
         
+        // 트리 초기화 
         for(int i = 0; i < info.length; i++)
             arr[i] = new ArrayList<>();
         
@@ -18,36 +19,33 @@ class Solution {
             arr[edge[1]].add(edge[0]);
         }
         
-        if(info[0] == 1) return 0;
-        for(int x: arr[0]) dq.add(x);
-        visited[0] = true; 
+        if(info[0] == 1) return 0; // 첫 노드가 1이면 바로 0 반환  
+        for(int x: arr[0]) dq.add(x); // 루트 노드에서 이동 가능한 경로 추가
+        visited[0] = true; // 루트 노드 방문 처리
         
-        dfs(1, 0, info);
+        dfs(1, 0, info); // dfs 
         return answer;
     }
-
-    //백 트랙킹, dfs  
+    
     void dfs(int sheep, int wolf, int[] info){
-        if(sheep > answer)
-            answer = sheep;
-        int num = dq.size();
-        for(int i = 0; i < num; i++){
+        if(sheep > answer) answer = sheep; // 최댓값 갱신 
+
+        // dfs 
+        for(int i = 0; i < dq.size(); i++){
             int x = dq.pollFirst();
-            if(!visited[x]){
+            int s = info[x]^1; // 다음 노드가 양인 경우  
+            int w = info[x]&1; // 다음 노드가 늑대인 경우 
+            if(!visited[x] && sheep + s > wolf + w){
                 visited[x] = true;
-                int count = 0;
-                for(int y: arr[x]){
+                int count = 0; // 다음 노드의 개수 
+                for(int y: arr[x]){ // 다음 노드 제거 
                     if(!visited[y]){
                         dq.addLast(y);
                         count++; 
                     }
                 }
-                int s = 0, w = 0;
-                if(info[x] == 0) s = 1;
-                else w = 1;
-                if(sheep + s > wolf + w)
-                    dfs(sheep + s, wolf + w, info);
-                for(int j = 0; j < count; j++)
+                dfs(sheep + s, wolf + w, info); // dfs 
+                for(int j = 0; j < count; j++) // 추가한 다음 노드 제거 
                     dq.removeLast();
                 visited[x] = false;   
             }
