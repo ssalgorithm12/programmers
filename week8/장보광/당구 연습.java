6682d89ae750db139ae9247e93a8dafa432c5c82
장보광 <154445682+JangBogwang@users.noreply.github.com>
2025-01-22 23:01:24 +0900

    해결/1.56ms

---
+++ week8/장보광/당구 연습.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int m, int n, int startX, int startY, int[][] balls) {
+        int[] answer = new int[balls.length];
+        
+        Arrays.fill(answer, Integer.MAX_VALUE);
+        for(int i = 0; i < balls.length; i++){
+            if(!(startX==balls[i][0] && startY <= balls[i][1]))
+                answer[i] = Math.min(answer[i], cal(startX, balls[i][0], startY, balls[i][1], n));                        
+            if(!(startX==balls[i][0]&&startY >= balls[i][1]))
+                answer[i] = Math.min(answer[i], cal(startX, balls[i][0], startY, balls[i][1], 0)); 
+            
+            if(!(startX <= balls[i][0]&&startY == balls[i][1]))
+                answer[i] = Math.min(answer[i], cal(startY, balls[i][1], startX, balls[i][0], m)); 
+            
+            if(!(startX >= balls[i][0] &&startY == balls[i][1]))
+                answer[i] = Math.min(answer[i], cal(startY, balls[i][1], startX, balls[i][0], 0)); 
+        }
+        
+        return answer;
+    }
+    
+    // 두 지점 사이의 거리를 계산하는 함수 
+    int cal(int a1, int a2, int b1, int b2, int x){
+        return (a1 - a2)*(a1 - a2) + (2*x - b1 - b2)*(2*x - b1 - b2);
+    }
+}
