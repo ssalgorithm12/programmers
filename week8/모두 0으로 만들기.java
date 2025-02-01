9f2c4a4b25bc308f10e559938c895d0103e943ac
Gaeun999999 <bge524@naver.com>
2025-02-01 19:23:38 +0900

    해결(수정)/255.28ms

--- week8/모두 0으로 만들기.java
+++ week8/모두 0으로 만들기.java
@@ -4,23 +4,22 @@ class Solution {
     static int n;
     static long[] val;
     static List<Integer>[] adj;
+    static int[] parent;
+    
     public long solution(int[] a, int[][] edges) {
         n = a.length;
         val = new long[n];
+        parent = new int[n];
         
         long sum = 0;
-        boolean flag = true;
         for(int i = 0; i < n; i++) {
             int tmp = a[i];
-            
             val[i] = tmp;
             sum += tmp;
-            if(tmp != 0 && flag) flag = false;
         }
         
         // 모든 가중치가 0인 경우, 불가능한 경우 처리
         if(sum != 0) return -1;
-        if(flag) return 0;
         
         adj = new List[n];
         for(int i = 0; i < n; i++) {
@@ -36,15 +35,17 @@ class Solution {
             adj[v].add(u);
         }
         
+        for(int i = 0; i < n; i++) {
+            parent[i] = adj[i].size();
+        }
+        
         return bfs();
     }
     
-    // 리프노드부터 시작해서 횟수 더하기
     static long bfs() {
         Queue<Integer> q = new LinkedList<>();
-        // 리프노드를 큐에 추가하기
         for(int i = 0; i < n; i++) {
-            if(adj[i].size() == 1) q.offer(i);
+            if(parent[i] == 1) q.offer(i);
         }
         
         boolean[] visited = new boolean[n];
@@ -56,11 +57,13 @@ class Solution {
             
             visited[cur] = true;
             ans += Math.abs(val[cur]);
+            parent[cur]--;
+            
             
             for(int a : adj[cur]) {
                 if(visited[a]) continue;
-                q.offer(a);
                 val[a] += val[cur];
+                if(--parent[a] == 1) q.offer(a);
             }
         }
         return ans;
--- week9/광고 삽입.java
+++ week9/광고 삽입.java
@@ -4,34 +4,38 @@ class Solution {
     public String solution(String play_time, String adv_time, String[] logs) {
         int len = convertToSecond(play_time);
         int[] time = new int[len + 1];
+        
+        // stack.size()로 개수 세기?
         for(int i = 0; i < logs.length; i++) {
             String[] s = logs[i].split("-");
             int start = convertToSecond(s[0]);
             int end = convertToSecond(s[1]);
-            // 다 표시하지 말고 시작, 끝만 표시?
+            
             for(int j = start; j <= end; j++) {
                 time[j]++;
             }
         }
         
-        int ad = convertToSecond(adv_time);
         int max = 0;
         int ans = 0;
         
+        int left = 0;
+        int right = convertToSecond(adv_time);
+        
         int cnt = 0;
-        for(int i = 0; i <= ad; i++) {
+        for(int i = left; i <= right; i++) {
             cnt += time[i];
         }
         
-        int t = 0;
+        // 슬라이딩 윈도우
         while(true) {
             if(cnt > max) {
                 max = cnt;
-                ans = t;
+                ans = left;
             }
-            if(t == len - ad) break;
-            cnt -= time[t++];
-            cnt += time[t + ad];
+            if(right == len) break;
+            cnt -= time[left++];
+            cnt += time[++right];
         }
         
         return convertToTime(ans);
@@ -40,8 +44,8 @@ class Solution {
     static String convertToTime(int time) {
         String h = String.valueOf(time / 3600);
         if(h.length() == 1) h = "0" + h;
-        
         time %= 3600;
+        
         String m = String.valueOf(time / 60);
         if(m.length() == 1) m = "0" + m;
         
